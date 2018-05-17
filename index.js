3f85314bff77b64ef51ea29164a2c12c628ff2a5
bagaspri22 <bagaspri22@gmail.com>
2018-05-17 21:35:59 +0700

    mengenal var, let, dan const

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var message = "hello Coding";
 let msg1 = "Hello";
-const msg2 = "Coding"
+
+const msg2 = "Coding";
+
+if(true){
+  var message = "1";
+  let msg1 = "2";
+  console.log("testing",msg1)
+}
 
 console.log(message)
+console.log(msg1)
