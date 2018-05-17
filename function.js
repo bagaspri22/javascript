f0543029bb168f6d8d645356a3e7ac8fb7626636
bagaspri22 <bagaspri22@gmail.com>
2018-05-17 21:47:13 +0700

    Penulisan funtion lama dan baru

---
+++ function.js
@@ -0,0 +1,16 @@
+function jalanSatu() {
+  console.log("jalan satu penulisan lama")
+}
+
+const jalanDua = function () {
+  console.log("jalan dua penulisan lama")
+}
+
+jalanSatu()
+jalanDua()
+
+const jalanTiga = ()=>{
+  console.log("jalan tiga penulisan baru")
+}
+
+jalanTiga()
