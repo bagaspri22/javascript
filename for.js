752737ddbdd2050ab1795e589a62fecb54fb332a
bagaspri22 <bagaspri22@gmail.com>
2018-05-24 15:04:21 +0700

    add switch.js , for.js , array.js

---
+++ for.js
@@ -0,0 +1,8 @@
+function perulangan() {
+  for(var i=1; i<10; i++){
+    console.log("jalan:",i)
+  }
+
+}
+
+perulangan()
---
+++ switch.js
@@ -0,0 +1,19 @@
+function testingSwitch(){
+  var nilai = "Dilo";
+
+  switch(nilai){
+    case "Dilo":
+      console.log("sangat bagus");
+      break;
+    case 8:
+      console.log("baik");
+      break;
+    case 6:
+      console.log("cukup");
+      break;
+    default:
+      console.log("lainnya");
+  }
+}
+
+testingSwitch()
